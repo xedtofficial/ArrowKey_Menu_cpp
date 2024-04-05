1531fa12cd11656a9d0eda822002ed8647ce478b
Xedt <113713880+xedtofficial@users.noreply.github.com>
2024-04-05 16:24:45 +0200

    Update menuclass.h

--- menuclass.h
+++ menuclass.h
@@ -121,7 +121,6 @@ class menu{
                                 cout<<"\n"<<titleVector[Titlecounter];
                             SetConsoleTextAttribute(hConsole, 07); // MODIFY for custom text color
                             for (int i=Titlecounter+1; i <=titleVector.size()-1; i++){
-                                cout<<"\n"<<titleVector[i];
                             }
                         }else if (i==VK_RIGHT){ // MODIFY for custom 'select'-key
                             system("cls");
